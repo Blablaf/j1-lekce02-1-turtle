41a32ab016e7391dc0a81109e8b252a81b3970f1
idome <i.domenyova@gmail.com>
2022-04-13 20:22:03 +0200

    zelvicka nakresli domecek vytvoreni vlastni metody nakresliDomecek

--- src/main/java/net/sevecek/turtle/HlavniProgram.java
+++ src/main/java/net/sevecek/turtle/HlavniProgram.java
@@ -2,6 +2,8 @@ package net.sevecek.turtle;
 
 import net.sevecek.turtle.engine.*;
 
+import java.awt.*;
+
 public class HlavniProgram {
 
     public void main(String[] args) {
@@ -9,25 +11,31 @@ public class HlavniProgram {
 
         zofka = new Turtle();
 
-        zofka.turnRight(45);
-        zofka.move(50);
-        zofka.turnRight(90);
-        zofka.move(50);
-        zofka.turnRight(45);
-        zofka.move(70);
-        zofka.turnRight(90);
-        zofka.move(70);
-        zofka.turnRight(90);
-        zofka.move(70);
-        zofka.turnRight(90);
-        zofka.move(70);
-
-
-
-
-
+        nakresliDomecek(zofka);
+        zofka.penUp();
+        zofka.move(100);
+        zofka.turnRight(270);
+        zofka.penDown();
+        zofka.setPenColor(Color.pink);
+        nakresliDomecek(zofka);
 
 
     }
+
+    private void nakresliDomecek(Turtle zofka) {
+        zofka.turnRight(45);
+        zofka.move(50);
+        zofka.turnRight(90);
+        zofka.move(50);
+        zofka.turnRight(45);
+        zofka.move(70);
+        zofka.turnRight(90);
+        zofka.move(70);
+        zofka.turnRight(90);
+        zofka.move(70);
+        zofka.turnRight(90);
+        zofka.move(70);
+    }
+
 
 }
